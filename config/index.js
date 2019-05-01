85cb3a12c2ed572dad90b3a218906ab760b74d49
Robb Moskv <fabrizio_39@hotmail.com>
2019-05-01 17:48:47 +0200

    moved models import into a config file to be imported at once

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const LaunchType = require('../models/Launch');
+const RocketType = require('../models/Rocket');
+const MissionType = require('../models/Mission');
+
+module.exports = {
+    LaunchType,
+    RocketType,
+    MissionType
+};
--- schema.js
+++ schema.js
@@ -1,6 +1,5 @@
-const LaunchType = require('./models/Launch');
-const RocketType = require('./models/Rocket');
-const MissionType = require('./models/Mission');
+
+const { LaunchType, RocketType, MissionType } = require('./config');
 
 const axios = require('axios');
 const {
